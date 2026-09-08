f2fab4705cec20f7ca6b7852b4e343fc38760dfb
Alesander126 <mateusz.s.info@gmail.com>
2026-09-08 18:01:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://nflxcqdjhpzajuzsmloc.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_zmFo6dHqVce06Eb694XUtw_GJuVXeSA"
+  SUPABASE_PUBLISHABLE_KEY: ""
 };
